ca9f430b44fe1e730e9a4376bbd9b83332e806ce
leOhsantos <leonardo.santos191004@gmail.com>
2022-08-12 14:22:24 -0300

    refatorando código js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,14 @@
 const navbar = document.getElementById("navbar"),
     logo = document.getElementById("logo"),
-    servicos = document.getElementsByClassName("navbar-links")[0],
-    projetos = document.getElementsByClassName("navbar-links")[1],
-    sobre = document.getElementsByClassName("navbar-links")[2],
-    contato = document.getElementsByClassName("navbar-links")[3];
+    links = document.querySelectorAll(".navbar-links");
 
 const activeScroll = () => {
     navbar.classList.toggle("navbar-ativo", scrollY > 0);
     logo.setAttribute("src", "assets/img/logo.png");
-    servicos.classList.toggle("navbar-links-ativo", scrollY > 0);
-    projetos.classList.toggle("navbar-links-ativo", scrollY > 0);
-    sobre.classList.toggle("navbar-links-ativo", scrollY > 0);
-    contato.classList.toggle("navbar-links-ativo", scrollY > 0);
+
+    links.forEach((e) => {
+        e.classList.toggle("navbar-links-ativo", scrollY > 0);
+    })
 
     if (scrollY === 0) {
         logo.setAttribute("src", "assets/img/logo-branca.png");
