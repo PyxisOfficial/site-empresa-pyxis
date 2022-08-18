d567bed02ba0e7e86550ec600bd7d10b5e2dad91
Yuri Oliveira <75813083+YuriOlivs@users.noreply.github.com>
2022-08-18 15:32:14 -0300

    atualizando a responsividade do site
    atualizando a responsividade da área de contato

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,13 +4,44 @@ const navbar = document.getElementById("navbar"),
     navbarList = document.querySelector(".navbar-list"),
     links = document.querySelectorAll(".navbar-links"),
     target = document.querySelectorAll("[data-anime]"),
-    form = document.getElementById("formContato"),
-    inputNome = document.getElementById("nomeUser"),
-    inputEmail = document.getElementById("emailUser"),
-    inputAssunto = document.getElementById("assuntoUser"),
-    inputMsg = document.getElementById("msgUser"),
-    errorText = document.getElementsByClassName("error-text"),
-    btnSubmit = document.getElementById("btnSubmit");
+    animationClass = "animate";
+
+const activeScroll = () => {
+    const windowTop = window.pageYOffset + (window.innerHeight * 0.75);
+
+    navbar.classList.toggle("navbar-ativo", scrollY > 0);
+    navbarList.classList.toggle("navbar-list-branco", scrollY > 0)
+
+    if (scrollY === 0) {
+        logo.setAttribute("src", "assets/img/logo-branca.png");
+    } else {
+        logo.setAttribute("src", "assets/img/logo.png");
+    }
+
+    links.forEach((e) => {
+        e.classList.toggle("navbar-links-ativo", scrollY > 0);
+    })
+
+    linesMenu.forEach((e) => {
+        e.classList.toggle("line-ativo", scrollY > 0);
+    })
+
+    target.forEach((e) => {
+        if (windowTop > e.offsetTop) {
+            e.classList.add(animationClass);
+        } else {
+            e.classList.remove(animationClass);
+        }
+    })
+
+    if (window.matchMedia("(max-width: 999px)").matches) {
+        target.forEach((e) => {
+            e.removeAttribute('data-anime');
+            document.getElementById('estrelas').classList.remove('estrelas_animation')
+        })
+    }
+}
+window.addEventListener("scroll", activeScroll);
 
 class MobileNavbar {
     constructor(mobileMenu, navList, navLinks) {
@@ -39,125 +70,7 @@ class MobileNavbar {
         this.mobileMenu.addEventListener("click", this.handleClick);
     }
 }
+
 const mobileNavbar = new MobileNavbar(".mobile-menu", ".navbar-list", ".navbar-list-item",);
 mobileNavbar.addClickEvent();
 
-const ativarScroll = () => {
-    const windowTop = window.pageYOffset + (window.innerHeight * 0.75);
-
-    navbar.classList.toggle("navbar-ativo", scrollY > 0);
-    navbarList.classList.toggle("navbar-list-branco", scrollY > 0)
-
-    if (scrollY === 0) {
-        logo.setAttribute("src", "assets/img/logo-branca.png");
-    } else {
-        logo.setAttribute("src", "assets/img/logo.png");
-    }
-
-    if (window.matchMedia("(max-width: 999px)").matches) {
-        target.forEach((e) => {
-            e.removeAttribute('data-anime');
-        })
-    }
-
-    links.forEach((e) => {
-        e.classList.toggle("navbar-links-ativo", scrollY > 0);
-    })
-
-    linesMenu.forEach((e) => {
-        e.classList.toggle("line-ativo", scrollY > 0);
-    })
-
-    target.forEach((e) => {
-        if (windowTop > e.offsetTop) {
-            e.classList.add("animate");
-        } else {
-            e.classList.remove("animate");
-        }
-    })
-}
-
-const validarCampos = () => {
-    let valueNome = inputNome.value,
-        regexEmail = /\S+@\S+\.\S+/,
-        emailTest = regexEmail.test(inputEmail.value),
-        valueAssunto = inputAssunto.value,
-        valueMsg = inputMsg.value;
-
-    if (valueNome === "") {
-        inputNome.setAttribute("class", "input-error");
-        errorText[0].classList.add("active");
-    } else {
-        inputNome.classList.remove("input-error");
-        errorText[0].classList.remove("active");
-    }
-
-    if (emailTest === false) {
-        inputEmail.setAttribute("class", "input-error");
-        errorText[1].classList.add("active");
-    } else {
-        inputEmail.classList.remove("input-error");
-        errorText[1].classList.remove("active");
-    }
-
-    if (valueAssunto === "") {
-        inputAssunto.setAttribute("class", "input-error");
-        errorText[2].classList.add("active");
-    } else {
-        inputAssunto.classList.remove("input-error");
-        errorText[2].classList.remove("active");
-    }
-
-    if (valueMsg === "") {
-        inputMsg.setAttribute("class", "input-error");
-        errorText[3].classList.add("active");
-    } else {
-        inputMsg.classList.remove("input-error");
-        errorText[3].classList.remove("active");
-    }
-
-    if (valueNome !== "" && emailTest === true && valueAssunto !== "" && valueMsg !== "") {
-        form.submit();
-    }
-}
-
-const removerErro = () => {
-    let valueNome = inputNome.value,
-        regexEmail = /\S+@\S+\.\S+/,
-        emailTest = regexEmail.test(inputEmail.value),
-        valueAssunto = inputAssunto.value,
-        valueMsg = inputMsg.value;
-
-    if (valueNome !== "") {
-        inputNome.classList.remove("input-error");
-        errorText[0].classList.remove("active");
-    }
-
-    if (emailTest !== false) {
-        inputEmail.classList.remove("input-error");
-        errorText[1].classList.remove("active");
-    }
-
-    if (valueAssunto !== "") {
-        inputAssunto.classList.remove("input-error");
-        errorText[2].classList.remove("active");
-    }
-
-    if (valueMsg !== "") {
-        inputMsg.classList.remove("input-error");
-        errorText[3].classList.remove("active");
-    }
-}
-
-document.addEventListener("keypress", () => {
-    if (window.event.keyCode === 13) {
-        btnSubmit.click();
-    }
-});
-
-window.addEventListener("scroll", ativarScroll);
-btnSubmit.addEventListener("click", validarCampos);
-inputNome.addEventListener("input", removerErro);
-inputEmail.addEventListener("input", removerErro);
-inputAssunto.addEventListener("input", removerErro);
-inputMsg.addEventListener("input", removerErro);
