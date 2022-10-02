1e675416dad2cf0c3ddd8b04cbc8dab91d1893b3
leOhsantos <leonardo.santos191004@gmail.com>
2022-10-02 13:49:03 -0300

    corrigindo splash

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,54 +1,56 @@
-const splash = document.querySelector('.splash'), 
-    bgEstrelas = document.getElementById("estrelas"),
-    nav = document.getElementById("nav"),
-    logo = document.getElementById("logo"),
-    mobileMenu = document.querySelector(".mobile-menu"),
-    navList = document.querySelector(".nav-list"),
-    navItems = document.querySelectorAll(".nav-list-item"),
-    linesMenu = document.querySelectorAll(".line"),
-    links = document.querySelectorAll(".nav-links"),
-    target = document.querySelectorAll("[data-anime]"),
-    form = document.getElementById("formContato"),
-    inputNome = document.getElementById("nomeUser"),
-    inputEmail = document.getElementById("emailUser"),
-    inputAssunto = document.getElementById("assuntoUser"),
-    inputMsg = document.getElementById("msgUser"),
-    errorText = document.getElementsByClassName("error-text"),
-    btnSubmit = document.getElementById("btnSubmit");
+const html = document.getElementsByTagName('html')[0],
+    splash = document.querySelector('.splash'),
+    bgEstrelas = document.getElementById('estrelas'),
+    nav = document.getElementById('nav'),
+    logo = document.getElementById('logo'),
+    mobileMenu = document.querySelector('.mobile-menu'),
+    navList = document.querySelector('.nav-list'),
+    navItems = document.querySelectorAll('.nav-list-item'),
+    linesMenu = document.querySelectorAll('.line'),
+    links = document.querySelectorAll('.nav-links'),
+    target = document.querySelectorAll('[data-anime]'),
+    form = document.getElementById('formContato'),
+    inputNome = document.getElementById('nomeUser'),
+    inputEmail = document.getElementById('emailUser'),
+    inputAssunto = document.getElementById('assuntoUser'),
+    inputMsg = document.getElementById('msgUser'),
+    errorText = document.getElementsByClassName('error-text'),
+    btnSubmit = document.getElementById('btnSubmit');
 
-document.addEventListener("DOMContentLoaded", (e)=>{
-    setTimeout(()=>{
-        splash.classList.add('nothing')
-    }, 3000)
-})
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        splash.classList.add('nothing');
+        html.removeAttribute('style');
+    }, 3000);
+});
 
 function handleMenu() {
-    navList.classList.toggle("active");
-    mobileMenu.classList.toggle("active");
+    navList.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
 
     navItems.forEach((link, index) => {
         link.style.animation
-            ? (link.style.animation = "")
+            ? (link.style.animation = '')
             : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
-    })
+    });
 }
 
 function changeNav() {
-    nav.classList.toggle("nav-ativo", scrollY > 0);
-    navList.classList.toggle("nav-list-branco", scrollY > 0);
+    nav.classList.toggle('nav-ativo', scrollY > 0);
+    navList.classList.toggle('nav-list-branco', scrollY > 0);
 
     if (scrollY === 0) {
-        logo.setAttribute("src", "assets/img/logos/pyxis-branca-sem-slogan.png");
+        logo.setAttribute('src', 'assets/img/logos/pyxis-branca-sem-slogan.png');
     } else {
-        logo.setAttribute("src", "assets/img/logos/pyxis-sem-slogan.png");
+        logo.setAttribute('src', 'assets/img/logos/pyxis-sem-slogan.png');
     }
 
     links.forEach((e) => {
-        e.classList.toggle("nav-links-ativo", scrollY > 0);
+        e.classList.toggle('nav-links-ativo', scrollY > 0);
     });
 
     linesMenu.forEach((e) => {
-        e.classList.toggle("line-ativo", scrollY > 0);
+        e.classList.toggle('line-ativo', scrollY > 0);
     });
 }
 
@@ -57,9 +59,9 @@ function animateElements() {
 
     target.forEach((e) => {
         if (windowTop > e.offsetTop) {
-            e.classList.add("animate");
+            e.classList.add('animate');
         } else {
-            e.classList.remove("animate");
+            e.classList.remove('animate');
         }
     });
 }
@@ -71,39 +73,39 @@ function validateInputs() {
         valueAssunto = inputAssunto.value,
         valueMsg = inputMsg.value;
 
-    if (valueNome === "") {
-        inputNome.setAttribute("class", "input-error");
-        errorText[0].classList.add("active");
+    if (valueNome === '') {
+        inputNome.setAttribute('class', 'input-error');
+        errorText[0].classList.add('active');
     } else {
-        inputNome.classList.remove("input-error");
-        errorText[0].classList.remove("active");
+        inputNome.classList.remove('input-error');
+        errorText[0].classList.remove('active');
     }
 
     if (emailTest === false) {
-        inputEmail.setAttribute("class", "input-error");
-        errorText[1].classList.add("active");
+        inputEmail.setAttribute('class', 'input-error');
+        errorText[1].classList.add('active');
     } else {
-        inputEmail.classList.remove("input-error");
-        errorText[1].classList.remove("active");
+        inputEmail.classList.remove('input-error');
+        errorText[1].classList.remove('active');
     }
 
-    if (valueAssunto === "") {
-        inputAssunto.setAttribute("class", "input-error");
-        errorText[2].classList.add("active");
+    if (valueAssunto === '') {
+        inputAssunto.setAttribute('class', 'input-error');
+        errorText[2].classList.add('active');
     } else {
-        inputAssunto.classList.remove("input-error");
-        errorText[2].classList.remove("active");
+        inputAssunto.classList.remove('input-error');
+        errorText[2].classList.remove('active');
     }
 
-    if (valueMsg === "") {
-        inputMsg.setAttribute("class", "input-error");
-        errorText[3].classList.add("active");
+    if (valueMsg === '') {
+        inputMsg.setAttribute('class', 'input-error');
+        errorText[3].classList.add('active');
     } else {
-        inputMsg.classList.remove("input-error");
-        errorText[3].classList.remove("active");
+        inputMsg.classList.remove('input-error');
+        errorText[3].classList.remove('active');
     }
 
-    if (valueNome !== "" && emailTest === true && valueAssunto !== "" && valueMsg !== "") {
+    if (valueNome !== '' && emailTest === true && valueAssunto !== '' && valueMsg !== '') {
         form.submit();
     }
 }
@@ -115,42 +117,42 @@ function removeInputsError() {
         valueAssunto = inputAssunto.value,
         valueMsg = inputMsg.value;
 
-    if (valueNome !== "") {
-        inputNome.classList.remove("input-error");
-        errorText[0].classList.remove("active");
+    if (valueNome !== '') {
+        inputNome.classList.remove('input-error');
+        errorText[0].classList.remove('active');
     }
 
     if (emailTest !== false) {
-        inputEmail.classList.remove("input-error");
-        errorText[1].classList.remove("active");
+        inputEmail.classList.remove('input-error');
+        errorText[1].classList.remove('active');
     }
 
-    if (valueAssunto !== "") {
-        inputAssunto.classList.remove("input-error");
-        errorText[2].classList.remove("active");
+    if (valueAssunto !== '') {
+        inputAssunto.classList.remove('input-error');
+        errorText[2].classList.remove('active');
     }
 
-    if (valueMsg !== "") {
-        inputMsg.classList.remove("input-error");
-        errorText[3].classList.remove("active");
+    if (valueMsg !== '') {
+        inputMsg.classList.remove('input-error');
+        errorText[3].classList.remove('active');
     }
 }
 
-if (window.matchMedia("(max-width: 999px)").matches) {
-    bgEstrelas.classList.remove("estrelas_animation");
+if (window.matchMedia('(max-width: 999px)').matches) {
+    bgEstrelas.classList.remove('estrelas_animation');
 }
 
-document.addEventListener("keypress", () => {
+document.addEventListener('keypress', () => {
     if (window.event.keyCode === 13) {
         btnSubmit.click();
     }
 });
 
-window.addEventListener("scroll", changeNav);
-window.addEventListener("scroll", animateElements);
-mobileMenu.addEventListener("click", handleMenu);
-btnSubmit.addEventListener("click", validateInputs);
-inputNome.addEventListener("input", removeInputsError);
-inputEmail.addEventListener("input", removeInputsError);
-inputAssunto.addEventListener("input", removeInputsError);
-inputMsg.addEventListener("input", removeInputsError);
+window.addEventListener('scroll', changeNav);
+window.addEventListener('scroll', animateElements);
+mobileMenu.addEventListener('click', handleMenu);
+btnSubmit.addEventListener('click', validateInputs);
+inputNome.addEventListener('input', removeInputsError);
+inputEmail.addEventListener('input', removeInputsError);
+inputAssunto.addEventListener('input', removeInputsError);
+inputMsg.addEventListener('input', removeInputsError);
